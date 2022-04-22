32f7eb2eae920ca63c7c6f0c21e57cb13b717bbf
heikki <sulinheikki@gmail.com>
2022-04-22 18:58:11 +0300

    Tweaks

--- config.h
+++ config.h
@@ -65,8 +65,9 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{disk_perc, 	"  %s%%  ", 		"/"},
-	{cpu_perc, 		" %s%%  ",  			NULL},
-	{ram_perc, 		"﬙ %s%%  ",  			NULL},
-	{datetime, 		"%s", 			"[ %F] [ %T]" },
+	{uptime,		" %s  ",			NULL},
+	{disk_perc, 	" %s%%  ", 		"/"},
+	{cpu_perc, 		" %s%%  ",  		NULL},
+	{ram_perc, 		"﬙ %s%%  ",  		NULL},
+	{datetime, 		"%s", 		     	"[ %F] [ %T]" },
 };
